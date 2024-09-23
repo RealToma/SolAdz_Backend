cb315e7b76b311932bb4be1deb97239a3623aeb2
RealToma <real.toma.contact@gmail.com>
2024-09-23 07:18:22 -0400

    updated all

--- src/utils/anchor.utils.ts
+++ src/utils/anchor.utils.ts
@@ -13,6 +13,24 @@ const program = new Program(IDL as Idl, provider);
 
 export const getReward = async (address: string) => {
     try {
+        const investorAccount = PublicKey.findProgramAddressSync(
+            [
+                utils.bytes.utf8.encode("investor"),
+                new PublicKey(address).toBuffer()
+            ],
+            program.programId
+        )[0];
+        const reward = await program.methods.rewardView().accounts({
+            investorAccount
+        }).view();
+        return Number(reward) / LAMPORTS_PER_SOL;
+    } catch (e) {
+        return 0;
+    }
+}
+
+export const getLastClaim = async (address: string) => {
+    try {
         const investor = PublicKey.findProgramAddressSync(
             [
                 utils.bytes.utf8.encode("investor"),
@@ -22,9 +40,9 @@ export const getReward = async (address: string) => {
         )[0];
         // @ts-ignore
         const investorAccount = await program.account.investor.fetch(investor);
-        return Number(investorAccount.amount) / LAMPORTS_PER_SOL;
+        return Number(investorAccount.lastUpdateCommission);
     } catch (e) {
-        return 0;
+        return Math.floor(Date.now() / 1000);
     }
 }
 
@@ -46,4 +64,32 @@ export const createClaimCommissionTxn = async (address: string, amount: number) 
     transaction.sign([signer.payer]);
     const serialzed = Buffer.from(transaction.serialize()).toString('base64');
     return serialzed;
+}
+
+export const processTopSponsorPool = async (address: string, amount: number) => {
+    const ixn = await program.methods.runDistribution().accounts({
+        receiver: new PublicKey(address)
+    }).signers([signer.payer]).instruction();
+    const instructions = [ixn];
+    const { blockhash } = await connection.getLatestBlockhash();
+    const message = new TransactionMessage({
+        payerKey: signer.publicKey,
+        recentBlockhash: blockhash,
+        instructions,
+    }).compileToV0Message();
+    const transaction = new VersionedTransaction(message);
+    const signed = await signer.signTransaction(transaction);
+    await connection.sendTransaction(signed);
+}
+
+export const getTopSponsorPoolAmount = async () => {
+    const appStats = PublicKey.findProgramAddressSync(
+        [
+            utils.bytes.utf8.encode("app-stats")
+        ],
+        program.programId
+    )[0];
+    // @ts-ignore
+    const statsAccount = await program.account.appStats.fetch(appStats);
+    return Number(statsAccount.topSponserPool) / LAMPORTS_PER_SOL;
 }
